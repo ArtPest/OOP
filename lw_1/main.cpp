88d807d6a827d803201366f8c570036b2bea10c7
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-09-17 23:05:54 +0300

    Create main.cpp

---
+++ lw_1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "turkish_numbers.h"
+
+using namespace std;
+
+int main(){
+    int num;
+    string str_num;
+    cin >> num;
+    number_translater(num, str_num);
+    cout << str_num << '\n';
+    return 0;
+}
