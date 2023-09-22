c8297756fae5438937cbd9f4896e38efb49f3c54
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-09-22 19:25:08 +0300

    Update palindrome.cpp

--- lw_1/palindrome.cpp
+++ lw_1/palindrome.cpp
@@ -1,8 +1,14 @@
 #include "palindrome.h"
+#include <cctype>
 
 void palindrome(string& str, bool& flag){
     int left = 0, right = str.length() - 1;
-    while (left < right)
-        if (str[left++] != str[right--])
+    while(left < right){
+        while(!isalnum(str[left++]));
+        while(!isalnum(str[right--]));
+        if(tolower(str[left++]) != tolower(str[right--])){
             flag = false;
+            return;
+        }
+    }
 }
