10ff1f35aee745c4d13948981e7798180723bb92
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-09-28 22:29:49 +0300

    Update four.cpp

--- lw_2/four.cpp
+++ lw_2/four.cpp
@@ -14,13 +14,13 @@ public:
             data[i] = t;
     }
     
-    Four(const initializer_list<unsigned char> &t): size(t.size()), data(new unsigned char[t.size()]){
+    /*Four(const initializer_list<unsigned char> &t): size(t.size()), data(new unsigned char[t.size()]){
         //
-    }
+    }*/
 
     Four(const string &t): size(t.length()), data(new unsigned char[t.length()]){
         for(size_t i = 0; i < t.length(); ++i)
-            data[i] = t[i];
+            data[i] = t[size - 1 - i] - '0';
     }
 
     Four(const Four& other): size(other.size), data(new unsigned char[other.size]){
@@ -38,10 +38,10 @@ public:
             delete[] data;
     }
     
-    void print(){
-        for(size_t i = size - 1; i != -1; --i)
-            cout << data[i];
-        cout << '\n';
+    void print() const{
+        for (size_t i = size - 1; i != -1; --i)
+            cout << static_cast<int>(data[i]);
+        cout << endl;
     }
     
     bool operator ==(const Four& other) const{
@@ -90,18 +90,24 @@ public:
     Four operator +(const Four& other) const{
         size_t max_s = max(size, other.size);
         Four result(max_s, 0);
-        unsigned char rem = 0;
+        unsigned char rema = 0;
         for(size_t i = 0; i < max_s; ++i){
-            int sum = rem;
+            int sum = rema;
             if(i < size)
                 sum += data[i];
             if(i < other.size)
                 sum += other.data[i];
             result.data[i] = sum % 4;
-            rem = sum / 4;
+            rema = sum / 4;
         }
-        if(rem){
-            //
+        if(rema){
+            unsigned char* tmp = new unsigned char[max_s + 1];
+            for(size_t i = 0; i < max_s; ++i)
+                tmp[i] = result.data[i];
+            tmp[max_s] = rema;
+            result.size = max_s + 1;
+            delete[] result.data;
+            result.data = tmp;
         }
         return result;
     }
