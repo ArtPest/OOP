55122481ede351be56edc3f4880bb28b1f364058
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-09-28 23:06:56 +0300

    Update four.cpp

--- lw_2/four.cpp
+++ lw_2/four.cpp
@@ -140,13 +140,3 @@ public:
         return result;
     }
 };
-
-int main(){
-    Four a("3333"), b("1000"), c("1"), d, e, f;
-    d = a + c;
-    e = b - c;
-    f = a - a;
-    d.print();
-    e.print();
-    f.print();
-}
