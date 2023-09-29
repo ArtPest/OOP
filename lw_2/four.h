a76fbddff77cd4deccf6b5ee3418f4e93058072f
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-09-29 11:03:54 +0300

    Create four.h

---
+++ lw_2/four.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <initializer_list>
+
+class Four {
+    unsigned char* data;
+    size_t size;
+
+public:
+    Four();
+    Four(const size_t &n, unsigned char t = 0);
+    //Four(const initializer_list<unsigned char>& t);
+    Four(const std::string &t);
+    Four(const Four& other);
+    Four(Four&& other) noexcept;
+    virtual ~Four() noexcept;
+
+    void print() const;
+    bool operator ==(const Four& other) const;
+    bool operator <(const Four& other) const;
+    bool operator >(const Four& other) const;
+    Four& operator =(const Four& other);
+    Four operator +(const Four& other) const;
+    Four operator -(const Four& other) const;
+};
