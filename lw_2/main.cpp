aa5ff0a4171f820e4ee2c93c4851ba15bdbbf7f8
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-09-29 22:31:50 +0300

    Update main.cpp

--- lw_2/main.cpp
+++ lw_2/main.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-#include "four.h"
-
-using namespace std;
-
 int main(){
-    Four a("3333"), b("1000"), c("1"), d, e, f;
+    Four a("3333"), b("1000"), c("1"), d, e, f(3, 2);
     d = a + c;
     e = b - c;
-    f = a - a;
+    /*try {
+        f = c - b;
+    } catch(const exception& exc){
+        cerr << "ERROR: " << exc.what() << '\n';
+        return -1;
+    }*/
     d.print();
     e.print();
     f.print();
