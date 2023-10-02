9b6f426382a8ee5706f0b470893e7000a4ec7817
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-10-02 18:46:11 +0300

    Update main.cpp

--- lw_2/main.cpp
+++ lw_2/main.cpp
@@ -7,14 +7,14 @@ int main(){
     Four a("3333"), b = {1, 0, 0, 0}, c("1"), d, e, f(3, 2);
     d = a + c;
     e = b - c;
-    /*try {
+    d.print();
+    e.print();
+    f.print();
+    try {
         f = c - b;
     } catch(const exception& exc){
         cerr << "ERROR: " << exc.what() << '\n';
         return -1;
-    }*/
-    d.print();
-    e.print();
-    f.print();
+    }
     cout << (a > b ? "YES" : "NO") << '\n';
 }
