406d48f7e48dfd4c49a2b70dd152a1f4c533a058
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-10-09 21:23:45 +0300

    Update figures.cpp

--- lw_3/figures.cpp
+++ lw_3/figures.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 
-class figure{
-    float x0, y0, area;
+class Figure{
+protected:
+    float x0, y0, area = -1;
 
 public:
+    Figure(float x, float y): x0(x), y0(y){}
     
-}
+    friend ostream& operator<<(std::ostream& os, const Figure& fig) {
+        return os << "Figure center: (" << fig.x0 << ", " << fig.y0 << ")\nArea: " << fig.area << '\n';
+    }
+};
 
 int main(){
+    Figure a(0, 0);
+    cout << a;
     return 0;
 }
