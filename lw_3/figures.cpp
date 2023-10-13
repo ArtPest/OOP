9b187de4e4810930984a5c9b5cfe591dec76f883
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-10-13 14:38:07 +0300

    Update figures.cpp

--- lw_3/figures.cpp
+++ lw_3/figures.cpp
@@ -2,22 +2,28 @@
 
 using namespace std;
 
-struct Point final{
-    float x, y;
-    
-    Point(float a = 0.0, float b = 0.0): x(a), y(b) {}
-    
-    friend ostream& operator<<(ostream& os, const Point& p){
-        return os << "(" << p.x << ", " << p.y << ")\n";
-    }
-};
+
 
 class Figure{
 protected:
+    struct Point final{
+        float x, y;
+        
+        Point(float a = 0.0, float b = 0.0): x(a), y(b) {}
+        
+        friend ostream& operator<<(ostream& os, const Point& p){
+            return os << "(" << p.x << ", " << p.y << ")\n";
+        }
+    };
+    
     Point a, b, c, d, o;
     float area = -1.0;
     
 public:
+    virtual ~Figure() noexcept = default;
+    
+    //explicit virtual operator double() const noexcept = 0;
+    
     friend ostream& operator<<(ostream& os, const Figure& f){
         return os << "Coordinates:\n" << f.a << f.b << f.c << f.d << "Center: " <<
             f.o << "Area: " << f.area << '\n';
