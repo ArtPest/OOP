a225d6e840bda4fa8ec9f453b09c2f6136ac3019
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-10-13 18:00:09 +0300

    Update figures.cpp

--- lw_3/figures.cpp
+++ lw_3/figures.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 struct Point final{
     float x, y;
-        
+
     Point(float a = 0.0, float b = 0.0): x(a), y(b) {}
         
     friend ostream& operator<<(ostream& os, const Point& p){
@@ -14,9 +14,21 @@ struct Point final{
 
 class Figure{
 protected:
+    int n;
     Point o;
     Point* vertices;
     float area = -1.0;
+    
+    Point center(){
+        Point result;
+        for(size_t i = 0; i < n; ++i){
+            result.x += vertices[i].x;
+            result.y += vertices[i].y;
+        }
+        result.x /= n;
+        result.y /= n;
+        return result;
+    }
     
 public:
     virtual ~Figure() noexcept = default;
@@ -25,22 +37,30 @@ public:
     
     friend ostream& operator<<(ostream& os, const Figure& f){
         if(f.vertices != NULL){
-            os << "Coordinates: ";
-            for(size_t i = 0; i < sizeof(vertices); ++i)
+            os << "Coordinates:\n";
+            for(size_t i = 0; i < f.n; ++i)
                 os << f.vertices[i];
         }
         os << "Center: " << f.o << "Area: " << f.area << '\n';
         return os;
     }
-
 };
 
-/*class Square: class Figure{
-}*/
+class Square final: public Figure{
+public:
+    Square(){
+        n = 4;
+        vertices = new Point[n];
+        o = center();
+    }
+    //Square(Point a, Point b, Point c, Point d) {}
+};
 
 
 int main(){
     Figure f;
+    Square s;
     cout << f;
+    cout << s;
     return 0;
 }
