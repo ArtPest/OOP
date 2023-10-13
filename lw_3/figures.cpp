9858602ec8cabf93637470044d603879e3233f3d
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-10-13 17:33:44 +0300

    Update figures.cpp

--- lw_3/figures.cpp
+++ lw_3/figures.cpp
@@ -2,21 +2,20 @@
 
 using namespace std;
 
-
+struct Point final{
+    float x, y;
+        
+    Point(float a = 0.0, float b = 0.0): x(a), y(b) {}
+        
+    friend ostream& operator<<(ostream& os, const Point& p){
+        return os << "(" << p.x << ", " << p.y << ")\n";
+    }
+};
 
 class Figure{
 protected:
-    struct Point final{
-        float x, y;
-        
-        Point(float a = 0.0, float b = 0.0): x(a), y(b) {}
-        
-        friend ostream& operator<<(ostream& os, const Point& p){
-            return os << "(" << p.x << ", " << p.y << ")\n";
-        }
-    };
-    
-    Point a, b, c, d, o;
+    Point o;
+    Point* vertices;
     float area = -1.0;
     
 public:
@@ -25,16 +24,18 @@ public:
     //explicit virtual operator double() const noexcept = 0;
     
     friend ostream& operator<<(ostream& os, const Figure& f){
-        return os << "Coordinates:\n" << f.a << f.b << f.c << f.d << "Center: " <<
-            f.o << "Area: " << f.area << '\n';
+        if(f.vertices != NULL){
+            os << "Coordinates: ";
+            for(size_t i = 0; i < sizeof(vertices); ++i)
+                os << f.vertices[i];
+        }
+        os << "Center: " << f.o << "Area: " << f.area << '\n';
+        return os;
     }
+
 };
 
 /*class Square: class Figure{
-    float length = -1.0;
-
-public:
-
 }*/
 
 
