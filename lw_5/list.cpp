c6eb5914e9e5e2585b7ba6337de87184782e6c3a
ArtPest <98692916+ArtPest@users.noreply.github.com>
2023-12-14 22:57:27 +0300

    Update list.cpp

--- lw_5/list.cpp
+++ lw_5/list.cpp
@@ -1,20 +1,14 @@
-#include<iostream>
-#include<cstdlib>
-#include<map>
+#include <iostream>
+#include <cstdlib>
+#include <map>
 
 using namespace std;
-
-template<typename T>
-struct Node {
-    T value;
-    Node<T>* next;
-};
 
 template<class T, size_t BLOCK_SIZE>
 class Allocator {
     T* _memory;
     size_t _memory_size;
-    
+
 public:
     static_assert(BLOCK_SIZE > 0);
     using value_type = T;
@@ -34,107 +28,143 @@ public:
         return &value; 
     }
 
-    Allocator(): _memory(new value_type[BLOCK_SIZE * sizeof(value_type)]), _memory_size(0) {}
+    Allocator(): _memory(reinterpret_cast<T*>(::operator new(BLOCK_SIZE * sizeof(T)))), _memory_size(0) {}
     
     ~Allocator() {
         delete[] _memory;
     }
 
-    pointer allocate(size_type num) {
-        if((_memory_size + num * sizeof(value_type)) > BLOCK_SIZE * sizeof(value_type))
+    pointer allocate(size_type num, const void* hint = 0) {
+        if (_memory_size + num > BLOCK_SIZE)
             throw bad_alloc();
-        pointer result = reinterpret_cast<pointer>(_memory + _memory_size);
-        _memory_size += num * sizeof(value_type);
+        pointer result = &_memory[_memory_size];
+        _memory_size += num;
         return result;
     }
 
     void construct(pointer p, const value_type& value) {
         new ((void*)p) value_type(value);
     }
-    
+
     void destroy(pointer p) {
-        p -> ~value_type();
+        p->~value_type();
     }
 
-    void deallocate(pointer p, size_type n) {
-        delete[] _memory;
-        _memory = new value_type[BLOCK_SIZE * sizeof(value_type)];
-        _memory_size = 0;
+    void deallocate(pointer p, size_type num) {
+        //?
+    }
+
+    size_type max_size() const {
+        return BLOCK_SIZE;
     }
 };
 
 template <typename T>
 class Iterator {
-    Node<T>* node;
-    
+    using Node = typename T::NodeType;
+    Node* ptr;
+
 public:
-    using value_type = T;
-    using pointer = T*;
-    using reference = T&;
+    using value_type = typename T::ValueType;
+    using pointer = typename T::ValueType*;
+    using reference = typename T::ValueType&;
     using difference_type = ptrdiff_t;
-    using iterator_category = forward_iterator_tag;
-    
-    Iterator(): node(nullptr) {}
-    
-    Iterator(Node<T>* _node): node(_node) {}
+    using iterator_category = bidirectional_iterator_tag;
+
+    Iterator(Node* p = nullptr): ptr(p) {}
 
     reference operator *() const {
-        return node -> value;
+        return ptr->value;
     }
 
     pointer operator ->() const {
-        return &(node -> value);
+        return &(ptr->value);
     }
 
     Iterator& operator ++() {
-        node = node -> next;
+        ptr = ptr->next;
         return *this;
     }
 
-    Iterator operator ++(int n) {
-        Iterator tmp(*this);
-        ++(*this);
+    Iterator operator ++(int) {
+        Iterator tmp(ptr);
+        operator ++();
+        return tmp;
+    }
+    
+    Iterator& operator --() {
+        if (ptr)
+            ptr = ptr->prev;
+        return *this;
+    }
+    
+    Iterator operator --(int) {
+        Iterator tmp(ptr);
+        operator --();
         return tmp;
     }
 
-    friend bool operator ==(const Iterator& lhs, const Iterator& rhs) {
-        return lhs.node == rhs.node;
+    bool operator ==(const Iterator& other) const {
+        return ptr == other.ptr;
     }
 
-    friend bool operator !=(const Iterator& lhs, const Iterator& rhs) {
-        return !(lhs == rhs);
+    bool operator !=(const Iterator& other) const {
+        return ptr != other.ptr;
     }
 };
 
-template<typename T, size_t Size, class Allocator>
+template<typename T, size_t BLOCK_SIZE, template<class, size_t> class Alloc>
 class List {
-    using NodeAllocator = typename Allocator::template rebind<Node<T>>::other;
+    struct Node {
+        T value;
+        Node* next;
+
+        Node(const T& v, Node* n): value(v), next(n) {}
+    };
+
+    using NodeAllocator = Alloc<Node, BLOCK_SIZE>;
+
     NodeAllocator allocator;
-    Node<T>* head;
-    
+    Node* head;
+
 public:
-    using iterator = Iterator<T>;
-    using const_iterator = Iterator<const T>;
-    
-    void push_front(T value) {
-        Node<T>* new_node = allocator.allocate(1);
-        allocator.construct(new_node, Node<T>{value, move(head)});
+    using NodeType = Node;
+    using ValueType = T;
+    using iterator = Iterator<List<T, BLOCK_SIZE, Alloc>>;
+    using const_iterator = Iterator<const List<T, BLOCK_SIZE, Alloc>>;
+
+    List(): head(nullptr) {}
+
+    ~List() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            allocator.destroy(temp);
+            //allocator.deallocate(temp, 1);
+        }
+    }
+
+    void push_front(const T& value) {
+        Node* new_node = allocator.allocate(1);
+        allocator.construct(new_node, Node(value, head));
         head = new_node;
     }
 
     void pop_front() {
-        Node<T>* old_head = head;
-        head = head -> next;
-        allocator.destroy(old_head);
-        allocator.deallocate(old_head, 1);
+        if (head) {
+            Node* old_head = head;
+            head = head->next;
+            allocator.destroy(old_head);
+            allocator.deallocate(old_head, 1);
+        }
     }
-    
+
     T& front() {
-        return head -> value;
+        return head->value;
     }
-    
+
     const T& front() const {
-        return head -> value;
+        return head->value;
     }
 
     iterator begin() {
@@ -145,27 +175,31 @@ public:
         return iterator(nullptr);
     }
 
-    const_iterator begin() const {
+    /*const_iterator begin() const {
         return const_iterator(head);
     }
 
     const_iterator end() const {
         return const_iterator(nullptr);
-    }
+    }*/
 };
+
 
 int main() {
     map<int, int, less<int>, Allocator<pair<const int, int>, 10>> m;
-    for(int i = 0; i < 5; ++i)
+    for (int i = 0; i < 5; ++i)
         m[i] = i * 10;
-    for(const auto& i: m)
-        cout << i.first << " " << i.second << '\n';
-    List<int, 10, Allocator<int, 10>> list;
+    for (const auto& p: m)
+        cout << p.first << " " << p.second << '\n';
+    
+    List<int, 10, Allocator> list;
     list.push_front(1);
     list.push_front(2);
     list.push_front(3);
-    for(auto it = list.begin(); it != list.end(); ++it)
+
+    for (auto it = list.begin(); it != list.end(); ++it)
         cout << *it << " ";
+    
     cout << '\n';
     return 0;
 }
